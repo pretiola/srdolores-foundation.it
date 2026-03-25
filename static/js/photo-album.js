adf9b90166a8d1fa1af3ba1df774d13a9db71db9
Jourdain-Alexander Casale <jourdain@indexexchange.com>
2026-03-24 22:35:34 -0400

    added subtle photo gallery effect

---
+++ static/js/photo-album.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("picture").forEach(function (pic) {
+    var angle = (Math.random() * 5 - 2.5);
+    pic.style.display = "block";
+    pic.style.transform = "rotate(" + angle.toFixed(2) + "deg)";
+    pic.style.filter = "drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.3))";
+  });
+});
